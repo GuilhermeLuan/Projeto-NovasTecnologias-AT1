35f2f5dbd9b952db655fd94aa4cd10d679f25676
Guilherme Luan <guilhermeluan@pm.me>
2025-06-18 20:30:10 -0300

    feat: add update functionality for todos with new update page and link integration

--- todoApp/todo_list/urls.py
+++ todoApp/todo_list/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('delete/<int:id>/', views.delete, name='delete'),
     path('done/<int:id>/', views.done, name='done'),
     path('undone/<int:id>/', views.undone, name='undone'),
+    path('update/<int:id>/', views.update, name='update'),
 ]
